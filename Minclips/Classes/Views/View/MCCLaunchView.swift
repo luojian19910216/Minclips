415ee6d5d457a04fe76207159ce46c280c927b26
Minclips <>
2026-04-30 03:34:55 -0700

    no message

--- Minclips/Classes/Views/View/MCCLaunchView.swift
+++ Minclips/Classes/Views/View/MCCLaunchView.swift
@@ -1,5 +1,20 @@
 import UIKit
+import SnapKit
 
 public class MCCLaunchView: MCCBaseView {
+    
+    public lazy var imageView: UIImageView = {
+        let v: UIImageView = .init()
+        v.image = .init(named: "AppLaunch")
+        return v
+    }()
+    
+    public override func mcvw_setupUI() {
+        self.addSubview(self.imageView)
+        self.imageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-40)
+        }
+    }
 
 }
